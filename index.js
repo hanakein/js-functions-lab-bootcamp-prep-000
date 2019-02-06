00f0676b412d01c42b7bde44f7e9afd90d1886f3
Hana Saori Keiningham <hanakein@gmail.com>
2019-02-06 20:49:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy Holidays, ${name}!"
+  return `Happy Holidays, ${name}!``
 }
 
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!
 }
 
-funtion holidayCountdown(holiday, days){
-  return "It's ${days} until ${holiday}!"
+funtion holidayCountdown(holiday, days) {
+  return `It\'s ${days} until ${holiday}!
 }
